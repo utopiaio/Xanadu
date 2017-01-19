f87de0512617f638a29f9c528f35347bcc9ba83d
Moe Szyslak <moe.duffdude@gmail.com>
2017-01-19 10:49:04 +0300

    webpack 2.0
    - vendor / css/js splitting 🙌🏿

--- webpack.config.js
+++ webpack.config.js
@@ -2,50 +2,68 @@ const webpack = require('webpack');
 const path = require('path');
 const autoprefixer = require('autoprefixer');
 const precss = require('precss');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const PRODUCTION = process.env.NODE_ENV === 'production';
+const commonPlugins = [
+  new webpack.LoaderOptionsPlugin({
+    options: {
+      postcss: [autoprefixer({ browsers: ['iOS >= 7'] }), precss],
+    },
+  }),
+  new ExtractTextPlugin({
+    filename: 'bundle.css',
+    disable: false,
+    allChunks: true,
+  }),
+  new webpack.optimize.CommonsChunkPlugin({
+    name: 'vendor',
+    filename: 'vendor.js',
+    minChunks: Infinity,
+  }),
+];
 
 module.exports = {
-  entry: ['./app/index.jsx'],
+  entry: {
+    vendor: ['react', 'react-dom'],
+    app: ['./app/index.jsx'],
+  },
   output: {
     path: path.join(__dirname, 'build'),
     filename: 'bundle.js',
   },
   resolve: {
-    root: path.resolve(__dirname),
-    alias: {
-      app: 'app',
-    },
-    extensions: ['', '.js', '.jsx', '.scss'],
+    alias: { App: path.resolve(__dirname, 'app/') },
+    extensions: ['.js', '.jsx', '.scss'],
   },
   module: {
-    loaders: [
+    rules: [
       // js[x]
-      { test: /\.jsx?$/, include: path.join(__dirname, 'app'), loader: 'babel-loader' },
+      { test: /\.jsx?$/, loader: 'babel-loader' },
 
       // css
-      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /\.css$/, use: ['style-loader', ExtractTextPlugin.extract('css-loader')] },
 
       // fonts
       { test: /\.(woff(2)?|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader?name=static/[name].[ext]' },
 
       // sass
-      { test: /\.scss$/, loader: 'style-loader!css-loader!postcss-loader!sass-loader' },
+      { test: /\.scss$/, use: ['style-loader', ExtractTextPlugin.extract('css-loader'), 'postcss-loader', 'sass-loader'] },
     ],
   },
-  postcss: [autoprefixer({ browsers: ['iOS >= 7'] }), precss],
-  devtool: PRODUCTION ? '#source-map' : null,
+  devtool: PRODUCTION ? 'source-map' : false,
   plugins: PRODUCTION ? [
-    new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify('production'),
       },
     }),
     new webpack.optimize.UglifyJsPlugin({
-      compressor: {
+      sourceMap: true,
+      compress: {
         warnings: false,
       },
-    }),
-  ] : [],
+    })
+  ].concat(commonPlugins) : [
+  ].concat(commonPlugins),
 };
