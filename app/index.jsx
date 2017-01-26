e92648c4b75841b5c19d945580104dc1c5ee965d
Moe Szyslak <moe.duffdude@gmail.com>
2017-01-26 18:55:31 +0300

    routes moved to index.jsx
    I'm going to stab you!

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,53 @@
 /* global document */
-
-import React from 'react';
+import React, { Component } from 'react';
+import { Route, Router, IndexRoute, browserHistory } from 'react-router';
+import { ThemeProvider } from 'styled-components';
 import { render } from 'react-dom';
 import 'normalize.css';
 
 require('App/scss/app.scss');
 
-import routes from 'App/config/routes.jsx';
+import { lightTheme, darkTheme } from 'App/config/theme.js';
+import App from 'App/components/hoc/App.jsx';
+import Index from 'App/components/presentational/Index.jsx';
+import AllTodos from 'App/components/hoc/AllTodos.jsx';
+import CurrentTodos from 'App/components/hoc/CurrentTodos.jsx';
+import Setting from 'App/components/hoc/Setting.jsx';
+import NotFound from 'App/components/presentational/NotFound.jsx';
 
-render(routes, document.querySelector('#app'));
+class Rafi extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      theme: '', // TODO: store.theme...
+    };
+  }
+
+  componentDidMount() {
+    // TODO: connect and Theme subscribe for ThemeProvider
+  }
+
+  componentWillUnmount() {
+    // TODO: unsubscribe from the store Luke
+  }
+
+  render() {
+    return (
+      <ThemeProvider theme={lightTheme}>
+        <Router history={browserHistory}>
+          <Route path="/" component={App}>
+            <IndexRoute component={Index} />
+            <Route path="/all" component={AllTodos} />
+            <Route path="/current" component={CurrentTodos} />
+            <Route path="/setting" component={Setting} />
+            <Route path="*" component={NotFound} />
+          </Route>
+        </Router>
+      </ThemeProvider>
+    );
+  }
+}
+
+render(<Rafi />, document.querySelector('#app'));
+
+module.exports = App;
