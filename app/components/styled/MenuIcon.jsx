62b9c938fb3217bfa0d4d2a65fa8981d7eda0ccd
Moe Szyslak <moe.duffdude@gmail.com>
2017-02-23 21:46:23 +0300

    proper icons, size and color

--- app/components/styled/MenuIcon.jsx
+++ app/components/styled/MenuIcon.jsx
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 const MenuIcon = styled.i`
-  margin-bottom: .25em;
-  font-size: 1.5em;
+  margin-bottom: .15em;
+  font-size: 1.6em;
 `;
 
 module.exports = MenuIcon;
