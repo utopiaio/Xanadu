037691418e29c6baee6dd63972b900ace5268d03
Moe Szyslak <moe.duffdude@gmail.com>
2017-02-24 00:32:22 +0300

    scroll view fix

--- app/components/styled/ScrollView.jsx
+++ app/components/styled/ScrollView.jsx
@@ -7,17 +7,20 @@ const ScrollView = styled.div`
   flex: 1 1 auto;
   height: ${props => props.height};
   overflow-y: ${props => props.overflowY};
+  overflow-x: ${props => props.overflowX};
   -webkit-overflow-scrolling: touch;
 `;
 
 ScrollView.propTypes = {
   height: PropTypes.string,
   overflowY: PropTypes.string,
+  overflowX: PropTypes.string,
 };
 
 ScrollView.defaultProps = {
   height: '100vh',
   overflowY: 'scroll',
+  overflowX: 'hidden',
 };
 
 module.exports = ScrollView;
