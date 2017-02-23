8829d142c2e40f2243b1305b0eb0004a2025a574
Moe Szyslak <moe.duffdude@gmail.com>
2017-02-24 01:14:30 +0300

    input component

---
+++ app/components/styled/Input.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Input = styled.input`
+  border: 1px solid red;
+  outline: none;
+  width: 100%;
+  font-size: 1.2em;
+  padding: 1.2em;
+  border-radius: 0;
+  border: none;
+  border-bottom: 1px solid #D0D0D0;
+  -webkit-appearance: none;
+`;
+
+module.exports = Input;
