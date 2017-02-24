84e80c18eb3742241944184df4a5d640923170f1
Moe Szyslak <moe.duffdude@gmail.com>
2017-02-25 00:34:03 +0300

    button now has a couple of more colors

--- app/components/styled/Button.jsx
+++ app/components/styled/Button.jsx
@@ -4,11 +4,12 @@ import styled from 'styled-components';
 const Button = styled.button`
   background-color: transparent;
   border-radius: 0.25em;
-  border: 1px solid #1da1f2;
+  border: 1px solid ${props => props.active ? '#1da1f2' : props.delete ? '#ff6d5e' : 'inherit'};
   padding: ${props => props.size === 'lg' ? '1.2em' : '0.8em'};
+  margin-bottom: ${props => props.size === 'lg' ? '1.2em' : '1em'};
   font-size: ${props => props.size === 'lg' ? '1.2em' : '1em'};
   width: 100%;
-  color: ${props => props.active ? '#1da1f2' : 'inherit'};
+  color: ${props => props.active ? '#1da1f2' : props.delete ? '#ff6d5e' : 'inherit'};
   opacity: ${props => props.disabled ? 0.5 : 1};
   outline: none;
 `;
