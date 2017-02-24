c2e34c438b1254319edfa7d8d0c18f504a5d7458
Moe Szyslak <moe.duffdude@gmail.com>
2017-02-24 12:41:53 +0300

    Haversine calculator

---
+++ app/util/haversine.js
@@ -0,0 +1,23 @@
+// eslint-disable-next-line
+const rad = x => x * Math.PI / 180;
+
+/**
+ * given two coordinates returns the distance in meters using Haversine
+ *
+ * @param {Object} p1 point 1
+ * @param {Number} p1.longitude
+ * @param {Number} p1.latitude
+ * @param {Object} p2 point 2
+ * @param {Number} p2.longitude
+ * @param {Number} p2.latitude
+ * @return {Number}
+ */
+module.exports = (p1, p2) => {
+  const R = 6378137; // Earth’s mean radius in meter
+  const dLat = rad(p2.latitude - p1.latitude);
+  const dLong = rad(p2.longitude - p1.longitude);
+  // eslint-disable-next-line
+  const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(rad(p1.lat())) * Math.cos(rad(p2.lat())) * Math.sin(dLong / 2) * Math.sin(dLong / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  return R * c;
+};
