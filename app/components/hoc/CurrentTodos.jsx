c62294cb78f0ff66727f95a65c2786ab0f38effe
Moe Szyslak <moe.duffdude@gmail.com>
2017-02-24 17:57:05 +0300

    Your forgiveness please 🇯🇵

--- app/components/hoc/CurrentTodos.jsx
+++ app/components/hoc/CurrentTodos.jsx
@@ -6,18 +6,22 @@ import { toggleAsync } from 'App/redux/actions/todo.js';
 import { currentTodo } from 'App/redux/selectors/todo.js';
 
 import ScrollView from 'App/components/styled/ScrollView.jsx';
-import Header from 'App/components/styled/Header.jsx';
+import { Header, HeaderMeter, HeaderTitle, HeaderLink } from 'App/components/styled/Header.jsx';
 import Task from 'App/components/presentational/Task.jsx';
 
 let CurrentTodos = ({ todos, location, range, toggle, edit }) => (
   <ScrollView height="calc(100vh - 50px)">
     <Header style={{ padding: '0 1em' }}>
-      <a style={{ flex: '0 0 64px', alignContent: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column', fontSize: '1.5em', textAlign: 'center' }}>
+      <HeaderMeter>
         <span className={location.accuracy > range ? 'warn' : ''}>{ location.accuracy.toLocaleString('us') }</span>
         <small style={{ fontSize: '0.4em' }}>Meters</small>
-      </a>
-      <a style={{ flex: '1 1 auto', alignContent: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column', fontSize: '1.5em', textAlign: 'center' }}>Xanadu</a>
-      <Link to="/new" style={{ flex: '0 1 64px', alignContent: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column', textDecoration: 'none', fontSize: '2em', color: '#31a8e6', textAlign: 'right' }}><i className="icon-circle-plus" /></Link>
+      </HeaderMeter>
+
+      <HeaderTitle >Xanadu</HeaderTitle>
+
+      <HeaderLink to="/new">
+        <i className="icon-circle-plus" />
+      </HeaderLink>
     </Header>
 
     <ScrollView height="calc(100vh - 114px)">
--- app/components/styled/Header.jsx
+++ app/components/styled/Header.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router';
 import styled from 'styled-components';
 
 const Header = styled.div`
@@ -7,4 +8,41 @@ const Header = styled.div`
   border-bottom: 1px solid #D0D0D0;
 `;
 
-module.exports = Header;
+const HeaderMeter = styled.div`
+  flex: 0 0 64px;
+  align-content: center;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+  font-size: 1.5em;
+  text-align: center;
+`;
+
+const HeaderTitle = styled.div`
+  flex: 1 1 auto;
+  align-content: center;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+  font-size: 1.5em;
+  text-align: center;
+`;
+
+const HeaderLink = styled(Link)`
+  flex: 0 1 64px;
+  align-content: center;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+  textDecoration: none;
+  fontSize: 2em;
+  color: #31a8e6;
+  text-align: right;
+`;
+
+module.exports = {
+  Header,
+  HeaderMeter,
+  HeaderTitle,
+  HeaderLink,
+};
