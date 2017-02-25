4c612a91d47d97febcc4332e4c75c5abbf225768
Moe Szyslak <moe.duffdude@gmail.com>
2017-02-25 11:44:57 +0300

    middleware for logging to badge

--- app/redux/store.js
+++ app/redux/store.js
@@ -3,6 +3,7 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
+import notificationLogger from 'App/redux/middleware/notification.js';
 import todo from 'App/redux/reducers/todo.js';
 import location from 'App/redux/reducers/location.js';
 import range from 'App/redux/reducers/range.js';
@@ -11,8 +12,8 @@ const store = createStore(
   combineReducers({ todo, location, range }),
   { todo: [], location: { accuracy: 1000, longitude: 0, latitude: 0 }, range: 100 },
   window.devToolsExtension
-    ? compose(applyMiddleware(thunk), window.devToolsExtension())
-    : applyMiddleware(thunk),
+    ? compose(applyMiddleware(thunk, notificationLogger), window.devToolsExtension())
+    : applyMiddleware(thunk, notificationLogger),
 );
 
 module.exports = store;
