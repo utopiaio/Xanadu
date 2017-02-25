68e852ec0d307ef48dc5fb3f297796a6dcc150df
Moe Szyslak <moe.duffdude@gmail.com>
2017-02-25 03:00:46 +0300

    setting

--- app/components/hoc/Setting.jsx
+++ app/components/hoc/Setting.jsx
@@ -1,18 +1,52 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
 
-class Setting extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+import { rangeAsync } from 'App/redux/actions/range.js';
 
-  render() {
-    return (<div>[Setting]</div>);
-  }
-}
+import ScrollView from 'App/components/styled/ScrollView.jsx';
+import { Header, HeaderTitle } from 'App/components/styled/Header.jsx';
+import { Setting, SettingRange, SettingButton } from 'App/components/styled/Setting.jsx';
 
-Setting.propTypes = {};
+let SettingComponent = ({ range, update }) => (
+  <ScrollView height="calc(100vh - 50px)">
+    <Header style={{ padding: '0 1em' }}>
+      <HeaderTitle>Setting</HeaderTitle>
+    </Header>
 
-Setting.defaultProps = {};
+    <ScrollView style={{ justifyContent: 'center', alignContent: 'center', alignItems: 'center' }} height="calc(100vh - 114px)">
+      <Setting>
+        <SettingButton onClick={() => update((range - 25) > 0 ? (range - 25) : 5)}>
+          <i className="icon-circle-minus" />
+        </SettingButton>
 
-module.exports = Setting;
+        <SettingRange>
+          <span>{range.toLocaleString('us')}</span>
+          <small style={{ fontSize: '0.4em' }}>Meters</small>
+        </SettingRange>
+
+        <SettingButton onClick={() => update((range + 25) > 1000 ? 1000 : range + 25)}>
+          <i className="icon-circle-plus" />
+        </SettingButton>
+      </Setting>
+    </ScrollView>
+  </ScrollView>
+);
+
+SettingComponent.propTypes = {
+  range: PropTypes.number,
+  update: PropTypes.func.isRequired,
+};
+
+SettingComponent.defaultProps = {
+  range: 100,
+};
+
+SettingComponent = connect(state => ({
+  range: state.range,
+}), dispatch => ({
+  update(r) {
+    dispatch(rangeAsync(r));
+  },
+}))(SettingComponent);
+
+module.exports = SettingComponent;
---
+++ app/components/styled/Setting.jsx
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+const Setting = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  font-size: 1.5em;
+  width: 75%;
+`;
+
+const SettingRange = styled.div`
+  display: flex;
+  flex: 1 1 auto;
+  flex-direction: column;
+  text-align: center;
+`;
+
+const SettingButton = styled.button`
+  flex: 1 1 auto;
+  background-color: transparent;
+  padding: 0 0.5em;
+  margin: 0;
+  font-size: 1.5em;
+  color: #7f8c8d;
+  border: none;
+`;
+
+module.exports = {
+  Setting,
+  SettingRange,
+  SettingButton,
+};
