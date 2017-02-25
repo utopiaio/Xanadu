6b8fec6091572585d75d4edc7cc4bcc92afa44d3
Moe Szyslak <moe.duffdude@gmail.com>
2017-02-25 04:30:05 +0300

    status bar adjustment

--- app/components/styled/Header.jsx
+++ app/components/styled/Header.jsx
@@ -11,33 +11,36 @@ const Header = styled.div`
 const HeaderMeter = styled.div`
   flex: 0 0 64px;
   align-content: center;
-  justify-content: center;
+  justify-content: flex-end;
   display: flex;
   flex-direction: column;
   font-size: 1.5em;
   text-align: center;
+  padding-bottom: 0.25em;
 `;
 
 const HeaderTitle = styled.div`
   flex: 1 1 auto;
   align-content: center;
-  justify-content: center;
+  justify-content: flex-end;
   display: flex;
   flex-direction: column;
   font-size: 1.5em;
   text-align: center;
+  padding-bottom: 0.5em;
 `;
 
 const HeaderLink = styled(Link)`
   flex: 0 1 64px;
   align-content: center;
-  justify-content: center;
+  justify-content: flex-end;
   display: flex;
   flex-direction: column;
   textDecoration: none;
   fontSize: 2em;
   color: #31a8e6;
   text-align: right;
+  padding-bottom: 0.25em;
 `;
 
 module.exports = {
