aff59a9dd69d1117a23866d26e884c8bf83b3456
Moe Szyslak <moe.duffdude@gmail.com>
2017-02-25 06:56:35 +0300

    moved around a bit of code --- redundant

--- app/components/hoc/App.jsx
+++ app/components/hoc/App.jsx
@@ -1,60 +1,34 @@
-import React, { Component, PropTypes } from 'react';
-import { Provider } from 'react-redux';
-
-import store from 'App/redux/store.js';
-import { bootLocation, watchLocation } from 'App/redux/actions/location.js';
-import { bootTodoAsync } from 'App/redux/actions/todo.js';
-import { bootRangeAsync } from 'App/redux/actions/range.js';
+import React, { PropTypes } from 'react';
 
 import { Menu, MenuLink, MenuIcon, MenuText } from 'App/components/styled/Menu.jsx';
 
-// booting with current location...
-store.dispatch(bootLocation());
-// starting location watcher...
-store.dispatch(watchLocation());
-// booting from LF...
-store.dispatch(bootTodoAsync());
-// booting from LF...
-store.dispatch(bootRangeAsync());
+const App = ({ children }) => (
+  <div className="xanadu-container">
+    <div className="xanadu-container__body">{ children }</div>
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+    <Menu className="xanadu-container__menu menu">
+      <MenuLink onlyActiveOnIndex to="/" activeClassName="menu_active">
+        <MenuIcon className="icon-archive" />
+        <MenuText>Xanadu</MenuText>
+      </MenuLink>
 
-  render() {
-    return (
-      <Provider store={store}>
-        <div className="xanadu-container">
-          <div className="xanadu-container__body">{ this.props.children }</div>
+      <MenuLink to="/current" activeClassName="menu_active">
+        <MenuIcon className="icon-location" />
+        <MenuText>Now</MenuText>
+      </MenuLink>
 
-          <Menu className="xanadu-container__menu menu">
-            <MenuLink onlyActiveOnIndex to="/" activeClassName="menu_active">
-              <MenuIcon className="icon-archive" />
-              <MenuText>Xanadu</MenuText>
-            </MenuLink>
+      <MenuLink to="/all" activeClassName="menu_active">
+        <MenuIcon className="icon-archive" />
+        <MenuText>All</MenuText>
+      </MenuLink>
 
-            <MenuLink to="/current" activeClassName="menu_active">
-              <MenuIcon className="icon-location" />
-              <MenuText>Now</MenuText>
-            </MenuLink>
-
-            <MenuLink to="/all" activeClassName="menu_active">
-              <MenuIcon className="icon-archive" />
-              <MenuText>All</MenuText>
-            </MenuLink>
-
-            <MenuLink to="/setting" activeClassName="menu_active">
-              <MenuIcon className="icon-cog" />
-              <MenuText>Setting</MenuText>
-            </MenuLink>
-          </Menu>
-        </div>
-      </Provider>
-    );
-  }
-}
+      <MenuLink to="/setting" activeClassName="menu_active">
+        <MenuIcon className="icon-cog" />
+        <MenuText>Setting</MenuText>
+      </MenuLink>
+    </Menu>
+  </div>
+);
 
 App.propTypes = {
   children: PropTypes.element.isRequired,
--- app/index.jsx
+++ app/index.jsx
@@ -2,11 +2,17 @@
 import React, { Component } from 'react';
 import { Route, Router, IndexRoute, hashHistory } from 'react-router';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 
 import 'normalize.css';
 
 import 'App/static/icon/style.css';
 import 'App/scss/app.scss';
+
+import store from 'App/redux/store.js';
+import { bootLocation, watchLocation } from 'App/redux/actions/location.js';
+import { bootTodoAsync } from 'App/redux/actions/todo.js';
+import { bootRangeAsync } from 'App/redux/actions/range.js';
 
 import App from 'App/components/hoc/App.jsx';
 import Index from 'App/components/hoc/Index.jsx';
@@ -33,21 +39,32 @@ class Xanadu extends Component {
 
   render() {
     return (
-      <Router history={hashHistory}>
-        <Route path="/" component={App}>
-          <IndexRoute component={Index} />
-          <Route path="/current" component={CurrentTodos} />
-          <Route path="/all" component={AllTodos} />
-          <Route path="/setting" component={Setting} />
-          <Route path="/new" component={New} />
-          <Route path="/edit/:id" component={Edit} />
-          <Route path="*" component={NotFound} />
-        </Route>
-      </Router>
+      <Provider store={store}>
+        <Router history={hashHistory}>
+          <Route path="/" component={App}>
+            <IndexRoute component={Index} />
+            <Route path="/current" component={CurrentTodos} />
+            <Route path="/all" component={AllTodos} />
+            <Route path="/setting" component={Setting} />
+            <Route path="/new" component={New} />
+            <Route path="/edit/:id" component={Edit} />
+            <Route path="*" component={NotFound} />
+          </Route>
+        </Router>
+      </Provider>
     );
   }
 }
 
 window.document.addEventListener('deviceready', () => {
+  // booting with current location...
+  store.dispatch(bootLocation());
+  // starting location watcher...
+  store.dispatch(watchLocation());
+  // booting from LF...
+  store.dispatch(bootTodoAsync());
+  // booting from LF...
+  store.dispatch(bootRangeAsync());
+
   render(<Xanadu />, document.querySelector('#app'));
 }, false);
