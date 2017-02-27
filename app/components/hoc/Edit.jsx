2d7e8a29b353fbf5ce8b39e178054d8237d4e885
Moe Szyslak <moe.duffdude@gmail.com>
2017-02-27 11:29:22 +0300

    removed connect on edit, fixing update while moving issue
    - no subscription required, one time store read, D.E.N.N.I.S. system in action people

--- app/components/hoc/Edit.jsx
+++ app/components/hoc/Edit.jsx
@@ -1,90 +1,94 @@
 /* global window */
-import React, { PropTypes } from 'react';
-import { connect } from 'react-redux';
+import React, { Component, PropTypes } from 'react';
 
 import { editAsync, removeAsync } from 'App/redux/actions/todo.js';
 
 import ScrollView from 'App/components/styled/ScrollView.jsx';
 import Button from 'App/components/styled/Button.jsx';
 
-let Edit = ({ task, location, update, remove }) => {
-  let input = null;
+class Edit extends Component {
+  constructor(props, context) {
+    super(props);
 
-  return (
-    <ScrollView height="calc(100vh - 50px)">
-      <form style={{ padding: '1em', paddingTop: '3em' }} onSubmit={(e) => { e.preventDefault(); update(task.id, input.value); input.value = ''; }}>
-        <input
-          type="text"
-          placeholder={task.task}
-          ref={(node) => { input = node; if (input !== null) { input.focus(); input.value = task.task; } }}
-          minLength="3"
-          maxLength="160"
-          required
-        />
-
-        <Button
-          active
-          size="md"
-          type="submit"
-        ><i className="icon-inbox" />&nbsp;&nbsp;Update Task</Button>
-
-        <Button
-          delete
-          size="md"
-          type="button"
-          onClick={() => remove(task.id)}
-        ><i className="icon-trash" /> Remove Task</Button>
-      </form>
-
-      <i style={{ textAlign: 'center', marginBottom: '.25em', fontSize: '2em' }} className="icon-location" />
-      <span style={{ textAlign: 'center' }}>Current accuracy within <b style={{ color: '#1da1f2' }}>{ window.parseInt(location.accuracy).toLocaleString('us')}</b> meters</span>
-    </ScrollView>
-  );
-};
-
-Edit.propTypes = {
-  task: PropTypes.shape({
-    id: PropTypes.number,
-    task: PropTypes.string,
-  }).isRequired,
-  location: PropTypes.shape({
-    accuracy: PropTypes.func.number,
-    longitude: PropTypes.func.number,
-    latitude: PropTypes.func.number,
-  }).isRequired,
-  update: PropTypes.func.isRequired,
-  remove: PropTypes.func.isRequired,
-};
-
-Edit = connect((state, props) => ({
-  task: (() => {
-    const taskId = Number(props.router.params.id);
+    const state = context.store.getState();
+    const taskId = Number(this.props.router.params.id);
     const task = state.todo.filter(todo => todo.id === taskId);
 
     if (task.length === 1) {
-      return {
+      this.state = {
         id: taskId,
         task: task[0].task,
+        location: state.location,
+      };
+    } else {
+      this.state = {
+        id: taskId,
+        task: task[0].task,
+        location: state.location,
       };
     }
+  }
 
-    return {
-      id: -1,
-      task: '',
-    };
-  })(),
-  location: state.location,
-}), (dispatch, props) => ({
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
   update(id, task) {
-    if (task.length > 2) {
-      dispatch(editAsync(id, task));
-      props.router.goBack();
-    }
-  },
+    this.context.store.dispatch(editAsync(id, task));
+    this.props.router.goBack();
+  }
+
   remove(id) {
-    dispatch(removeAsync(id));
-    props.router.goBack();
-  },
-}))(Edit);
+    this.context.store.dispatch(removeAsync(id));
+    this.props.router.goBack();
+  }
+
+  render() {
+    return (
+      <ScrollView height="calc(100vh - 50px)">
+        <form
+          style={{ padding: '1em', paddingTop: '3em' }}
+          onSubmit={(e) => { e.preventDefault(); this.update(this.state.id, this.state.task); }}
+        >
+          <input
+            type="text"
+            name="task"
+            placeholder={this.state.task}
+            value={this.state.task}
+            onChange={e => this.handleChange(e)}
+            minLength="3"
+            maxLength="160"
+            required
+          />
+
+          <Button
+            active
+            size="md"
+            type="submit"
+          ><i className="icon-inbox" />&nbsp;&nbsp;Update Task</Button>
+
+          <Button
+            delete
+            size="md"
+            type="button"
+            onClick={() => this.remove(this.state.id)}
+          ><i className="icon-trash" /> Remove Task</Button>
+        </form>
+
+        <i style={{ textAlign: 'center', marginBottom: '.25em', fontSize: '2em' }} className="icon-location" />
+        <span style={{ textAlign: 'center' }}>Current accuracy within <b style={{ color: '#1da1f2' }}>{ window.parseInt(this.state.location.accuracy).toLocaleString('us')}</b> meters</span>
+      </ScrollView>
+    );
+  }
+}
+
+Edit.propTypes = {
+  router: PropTypes.shape().isRequired,
+};
+
+Edit.contextTypes = {
+  store: PropTypes.object,
+};
 
 module.exports = Edit;
+
